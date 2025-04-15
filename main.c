b6093f5e2007f56e318d70618bcdbf53197ff03a
WSL-Home-Desktop <ChrisJacobBoon@gmail.com>
2025-04-15 14:39:34 -0400

    Small cleanup of variable names

--- main.c
+++ main.c
@@ -5,11 +5,12 @@
 int main(int argc, char *argv[])
 {
     // level_t one = {0};
-    level_t one = new_level("levels/level_one.txt","Intro: Hello World!","Prompt: What do you like?",1);
+    char *intro_l1 = "Intro: Hello World!";
+    char *prompt_l1 = "Prompt: What do you like?";
+    level_t one = new_level("levels/level_one.txt",intro_l1,prompt_l1,1);
 
     int status = print_ascii(one);
     printf("Print ascii exit code: %d\n",status);
-
 
     return 0;
 }
