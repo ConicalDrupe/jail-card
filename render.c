ee9070a925ebc7145f695775cd067b20e0ce0e62
ConicalDrupe <ChrisJacobBoon@gmail.com>
2025-04-22 23:27:03 -0400

    Added padding for rendering cards. NEXT: add padding for game options: call/play/fold

--- render.c
+++ render.c
@@ -28,13 +28,34 @@ static char four_art[4][6] = {"*---*","| 4 |","|   |","*---*"};
 static char five_art[4][6] = {"*---*","| 5 |","|   |","*---*"};
 static char unkn_art[4][6] = {"*---*","| ? |","|   |","*---*"};
 
-// Left Padding for number of cards. 1 Card, 2 Cards, 3 Cards, 4 Cards, 5 Cards
-char *one_card_pad = "";
-char *two_card_pad = "";
-char *three_card_pad = "";
-char *four_card_pad = "";
+// TWO OPTIONS FOR PADDING. 1) string cat 2) Pointer fun in canvas array.
+// StringCat uses more RAM, because we copy two strings in memory into a new string
+// Pointer method uses less memory, because we just alter the values we already initialized
+//one card, 36 whitespaces
+//two cards, 33 whitespaces
+//three cards, 30 whitespaces
+//four cards, 27 whitespaces
+//five cards, 24 whitespaces
+static char oneCardPad[37] = "                                    ";
+static char twoCardPad[34] = "                                 ";
+static char threeCardPad[31] = "                              ";
+static char fourCardPad[28] = "                           ";
+static char fiveCardPad[25] = "                        ";
 
 // Left padding for game board option rendering. 2 options (call or fold), 3 options (call, play or fold)
+// 28 white spaces, then 9 between call/fold. 26 for (-5 coins), then 6 spaces to (-1 card)
+//call,fold
+static char lpad_two_opts[29] = "                            ";
+static char between_two_opts[10] = "         ";
+static char sub_lpad_two_opts[27] = "                          ";
+static char sub_between_two_opts[7] = "      ";
+//call,play,fold
+static char lpad_three_opts[23] = "                      ";
+static char between_three_opts[8] = "       ";
+static char sub_lpad_three_opts[21] = "                    ";
+static char sub_between_three_opts[4] = "   ";
+// 22 white spaces, then 7 between each
+// 20 white spaces til (-5 coins), then 3 till each (play card) and (-1 card)
 
 
 // User for O(n) time complexity
@@ -61,6 +82,26 @@ void renderCards(card_t cards_arry[], int num_of_cards, int prompt_flag) {
             // printf("%s",oponent_pad)
             // printf("%s",player_pad)
             // printf("%s",options_pad)
+            switch (num_of_cards) {
+                case 1:
+                    printf("%s",oneCardPad);
+                    break;
+                case 2:
+                    printf("%s",twoCardPad);
+                    break;
+                case 3:
+                    printf("%s",threeCardPad);
+                    break;
+                case 4:
+                    printf("%s",fourCardPad);
+                    break;
+                case 5:
+                    printf("%s",fiveCardPad);
+                    break;
+                default:
+                    printf("\n\n\n\n");
+                    break;
+            }
 
             // Print card line by line
             switch (current_card.name) {
