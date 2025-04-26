ff39f68adb1c359ff6cc68c036c39480a4d92c81
WSL-Home-Desktop <ChrisJacobBoon@gmail.com>
2025-04-25 23:25:52 -0400

    Corrected remove card and add card, using pointers (*card_t) as function inputs, and -> to access data directly. Added addCard() and removeCard() tests

--- player.c
+++ player.c
@@ -5,51 +5,56 @@ typedef struct Card card_t;
 
 typedef struct Player {
     card_t inventory[5];
-    char name[10];
+    char *name;
     int chips;
     int num_cards;
     unsigned int seed;
 } player_t;
 
 // MUST INSTATIATE PLAYER, else we get bad values for chips/num_cards by default.
-void createPlayer(char[10] name) {
-    player_t player = {.name=name};
+player_t createPlayer(char *name) {
+    player_t player = {.name = name};
     return player;
 }
 
-void addCard(player_t player,card_t card){
-    if (player.num_cards < 5) {
-        player.inventory[player.num_cards] = card;
-        player.num_cards++;
+void addCard(player_t *player,card_t card){
+    if (player->num_cards < 5) {
+        player->inventory[player->num_cards] = card;
+        // int *nCards_ptr = &player.num_cards;
+        player->num_cards++;
+        // printf("From struct directly: %d\n",player->num_cards);
+        // printf("From pointer: %d\n",*nCards_ptr);
+        // player.num_cards = player.num_cards + 1;
     }
     else {
-        printf("Inventory is full");
+        printf("Inventory is full\n");
     }
 }
 
-void removeCard(player_t player) {
-    int n = player.num_cards;
-    if (n > 1) {
+void removeCard(player_t *player) {
+    int n = player->num_cards;
+    if (n > 0) {
         int idx_to_remove = rand() % n;
+        printf("removeCard() chose index %d\n",idx_to_remove);
         if (idx_to_remove == n-1) {
             // card_t *ptr_to_card = &player.inventory[n-1];
             // *ptr_to_card = createCard(100);//{0};
-            player.inventory[n-1] = createCard(100);
-            player.num_cards--;
+            player->inventory[n-1] = createCard(100);
+            player->num_cards--;
         }
         else {
             // If not the last card in inventory, swap with last card & clear marked card.
-            card_t *temp = &player.inventory[n-1]; //last card in inventory
-            card_t *ptr_to_card = &player.inventory[idx_to_remove];
+            card_t *temp = &player->inventory[n-1]; //last card in inventory
+            card_t *ptr_to_card = &player->inventory[idx_to_remove];
             *ptr_to_card = *temp;
-            player.inventory[n-1] = createCard(100); //removing dulpicate card
+            player->inventory[n-1] = createCard(100); //removing dulpicate card
             // *temp = createCard(100);
-            player.num_cards--;
+            player->num_cards--;
         }
     }
     else {
-        printf("%s has no cards to remove\n",player.name);
-        printf("Player has %d cards\n",player.num_cards);
+        printf("%s has no cards to remove\n",player->name);
+        printf("Player has %d cards\n",player->num_cards);
     }
 };
 
--- tests/test_player.c
+++ tests/test_player.c
@@ -5,19 +5,37 @@
 int main()
 {
     // IMPORTANT! Instantiate every player, so they start with 0 chips and 0 cards.
-    player_t player = {0};
+    player_t player = createPlayer("PlayerTest");
 
     printf("player.num_cards %d\n",player.num_cards);
-    // player.inventory[0] = createCard(1);
-    card_t temp = generateCard();
 
-    // card_t temp = createCard(7); //bug
-    // player.inventory[0] = createCard(7);
-    player.num_cards++;
-
-
-    addCard(player,temp);
+    addCard(&player,generateCard());
     renderCards(player.inventory,1);
+    printf("New Number of cards after addCard(): %d\n",player.num_cards);
+
+    removeCard(&player);
+    printf("New Number of cards after removeCard(): %d\n",player.num_cards);
+
+    printf("Displaying 1 cards:\n");
+    renderCards(player.inventory,1);
+
+    ////////////////////////////////////////////
+    printf("\n\n\n");
+    player_t player2 = createPlayer("PlayerTest2");
+
+    printf("player2.num_cards %d\n",player2.num_cards);
+
+    addCard(&player2,generateCard());
+    addCard(&player2,generateCard());
+    addCard(&player2,generateCard());
+    renderCards(player2.inventory,player2.num_cards);
+    printf("New Number of cards after addCard(): %d\n",player2.num_cards);
+
+    removeCard(&player2);
+    printf("New Number of cards after removeCard(): %d\n",player2.num_cards);
+
+    printf("Displaying 3 cards:\n");
+    renderCards(player2.inventory,player2.num_cards+1);
 
     return 0;
 }
